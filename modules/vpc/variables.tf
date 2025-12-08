cd2d713d476d7e9eb7bd016c5d2f704484f5e4ee
kofiad <kofiadaba@gmail.com>
2025-12-08 16:10:02 +0000

    refactor: update CIDR block generation to use VPC prefix length

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -33,15 +33,18 @@ variable "private_subnet_b_ips" {
 }
 
 locals {
+  # Extract VPC prefix length from CIDR
+  vpc_prefix = tonumber(split("/", var.vpc_cidr)[1])
+  
   # Calculate prefix length based on required IP addresses
   public_a_prefix = 32 - ceil(log(var.public_subnet_a_ips, 2))
   public_b_prefix = 32 - ceil(log(var.public_subnet_b_ips, 2))
   private_a_prefix = 32 - ceil(log(var.private_subnet_a_ips, 2))
   private_b_prefix = 32 - ceil(log(var.private_subnet_b_ips, 2))
 
-  # Generate CIDR blocks
-  public_subnet_a_cidr = cidrsubnet(var.vpc_cidr, local.public_a_prefix - 16, 0)
-  public_subnet_b_cidr = cidrsubnet(var.vpc_cidr, local.public_b_prefix - 16, 1)
-  private_subnet_a_cidr = cidrsubnet(var.vpc_cidr, local.private_a_prefix - 16, 2)
-  private_subnet_b_cidr = cidrsubnet(var.vpc_cidr, local.private_b_prefix - 16, 3)
+  # Generate CIDR blocks (newbits = target_prefix - vpc_prefix)
+  public_subnet_a_cidr = cidrsubnet(var.vpc_cidr, local.public_a_prefix - local.vpc_prefix, 0)
+  public_subnet_b_cidr = cidrsubnet(var.vpc_cidr, local.public_b_prefix - local.vpc_prefix, 1)
+  private_subnet_a_cidr = cidrsubnet(var.vpc_cidr, local.private_a_prefix - local.vpc_prefix, 2)
+  private_subnet_b_cidr = cidrsubnet(var.vpc_cidr, local.private_b_prefix - local.vpc_prefix, 3)
 }
