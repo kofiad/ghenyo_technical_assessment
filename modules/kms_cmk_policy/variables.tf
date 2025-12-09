0688c3eadc46be42a7d9fefcfce4befe47981762
kofiad <kofiadaba@gmail.com>
2025-12-09 16:34:24 +0000

    format: adjust variable definitions for consistency in kms_cmk_policy module

--- modules/kms_cmk_policy/variables.tf
+++ modules/kms_cmk_policy/variables.tf
@@ -1,8 +1,9 @@
 variable "kms_key_id" {
   description = "ID of kms key"
-  type = string
+  type        = string
 }
 
 variable "environment" {
-  
+  description = "Environment name for resource tagging and access control"
+  type        = string
 }
