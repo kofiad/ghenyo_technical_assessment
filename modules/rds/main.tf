63dc87aee0abce082c2a05b91ab358fe25d75302
kofiad <kofiadaba@gmail.com>
2025-12-09 16:34:47 +0000

    refactor: restructure RDS module for improved organization and clarity

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,19 +1,45 @@
-terraform {
-  required_version = ">= 1.0.0, < 2.0.0"
+resource "aws_security_group" "rds_sg" {
+  name = "${var.name}-sg"
+  description = "${var.name} rds instance security group"
+  vpc_id = var.vpc_id
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  ingress{
+    description = "Allow database traffic on its port"
+    from_port = var.database_port
+    to_port = var.database_port
+    protocol = "tcp"
+    cidr_blocks = var.allowed_cidr_block #private subnet cidr
+  }
+
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_db_instance" "example" {
-  identifier_prefix   = "ghenyo-ta"
-  engine              = "mysql"
-  allocated_storage   = 10
-  instance_class      = "db.t2.micro"
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.name}-db-sng"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name = "${var.name}-db-sng"
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier   = var.name
+  engine              = var.db_engine
+  engine_version = var.engine_version
+  allocated_storage   = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  instance_class      = var.db_instance_class
+  storage_encrypted = true
+  kms_key_id = var.kms_key_id
+  publicly_accessible = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.main.name
   db_name             = var.db_name
   username            = var.db_username
   password            = var.db_password
