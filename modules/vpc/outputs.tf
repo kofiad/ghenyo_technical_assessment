1f10aab274db8f0b901723357286117c1f9a608e
kofiad <kofiadaba@gmail.com>
2025-12-09 16:34:52 +0000

    fix: correct output definitions for public and private subnet CIDR blocks

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -13,9 +13,9 @@ output "public_subnet_a_id" {
   value       = aws_subnet.public-a.id
 }
 
-output "public_subnet_b_id" {
-  description = "ID of public subnet B"
-  value       = aws_subnet.public-b.id
+output "public_subnet_a_cidr" {
+  description = "ID of public subnet A"
+  value       = aws_subnet.public-a.cidr_block
 }
 
 output "private_subnet_a_id" {
@@ -23,9 +23,9 @@ output "private_subnet_a_id" {
   value       = aws_subnet.private-a.id
 }
 
-output "private_subnet_b_id" {
-  description = "ID of private subnet B"
-  value       = aws_subnet.private-b.id
+output "private_subnet_a_cidr" {
+  description = "ID of private subnet A"
+  value       = aws_subnet.private-a.cidr_block
 }
 
 output "nat_gateway_id" {
