fb65ad69ebc7b72954ce8b1cdda214048c2c1b43
kofiad <kofiadaba@gmail.com>
2025-12-09 16:33:28 +0000

    update: correct ALB output values and descriptions in outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,14 +1,19 @@
 output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
+  value       = aws_lb.main.dns_name
   description = "The domain name of the load balancer"
 }
 
 output "alb_http_listener_arn" {
-  value       = aws_lb_listener.http.arn
-  description = "The ARN of the HTTP listener"
+  value       = aws_lb_listener.http_to_https.arn
+  description = "The ARN of the HTTP redirect listener"
+}
+
+output "alb_https_listener_arn" {
+  value       = aws_lb_listener.https.arn
+  description = "The ARN of the HTTPS listener"
 }
 
 output "alb_security_group_id" {
-  value       = aws_security_group.alb.id
+  value       = aws_security_group.lb_sg.id
   description = "The ALB Security Group ID"
 }
