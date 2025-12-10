f96e3dd82894773ccc9c4028f934d711175452db
kofiad <kofiadaba@gmail.com>
2025-12-10 12:41:49 +0000

    fix: correct type declaration for private_subnet_a_cidr variable

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -15,7 +15,7 @@ variable "public_subnet_a_cidr" {
 
 variable "private_subnet_a_cidr" {
   description = "Number of IP addresses required for private subnet A"
-  type        = stringcl
+  type        = string
 }
 
 variable "private_subnet_b_cidr" {
